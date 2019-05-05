ec6cac0952655d82650ccdea20d9bba3ceec511d
Kit Jonathan Te <kjonathante@gmail.com>
2019-05-04 23:47:07 -0700

    Added http package. Created a server and use the service

--- bin/run.js
+++ bin/run.js
@@ -1,4 +1,20 @@
+"use stric";
+
 const config = require("../config");
 const log = config.log();
 
 log.info(config.googleId);
+
+const service = require("../server/service"); //(config);
+const http = require("http");
+const server = http.createServer(service);
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT);
+
+server.on("listening", function() {
+  log.info(
+    `listening on ${server.address().port} in ${service.get("env")} mode.`
+  );
+});
